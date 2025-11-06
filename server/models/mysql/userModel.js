ab1575ff5716ec4efc2e4cc36bc28f329836a696
Arianna <ariigrangetto5@gmail.com>
2025-11-05 23:23:30 -0300

    feat: new mySql querys

--- server/models/mysql/userModel.js
+++ server/models/mysql/userModel.js
@@ -38,9 +38,118 @@ export class UserModel {
     return result;
   };
 
-  static createUser = (user) => {};
+  static createUser = async (user) => {
+    console.log(typeof user);
+    const {
+      firstName,
+      lastName,
+      maidenName,
+      age,
+      gender,
+      email,
+      phone,
+      username,
+      password,
+      birthDate,
+      image,
+      height,
+      weight,
+      eyeColor,
+      hair,
+      university,
+      role,
+    } = user;
 
-  static updateUser = (id, updateUser) => {};
+    //para poder obtener el id porque por más que sea random y que se puede generar solo, luego es imposible poder recuperarlo;
 
-  static deleteUser = (id) => {};
+    //llamando a sql para generar el uuid;
+    const [uuidResult] = await connection.query("SELECT UUID() uuid;");
+    const [{ uuid }] = uuidResult;
+
+    try {
+      await connection.query(
+        `
+      INSERT INTO users (user_id, user_firstName, user_lastName, user_maidenName, user_age, user_gender, user_email, user_phone, user_username, user_password, user_birthDate, user_image, user_height, user_weight, user_eyeColor, user_hair_type, user_hair_color, user_university, user_rol)
+      VALUES (UUID_TO_BIN(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+        [
+          uuid,
+          firstName,
+          lastName,
+          maidenName,
+          age,
+          gender,
+          email,
+          phone,
+          username,
+          password,
+          birthDate,
+          image,
+          height,
+          weight,
+          eyeColor,
+          hair?.type,
+          hair?.color,
+          university,
+          role,
+        ]
+      );
+    } catch (e) {
+      //evitar mandar informacion sensible al usuario
+      throw new Error("Error creating new user: " + e.message);
+    }
+
+    const [userCreated] = await connection.query(
+      `SELECT BIN_TO_UUID(user_id) user_id, user_firstName, user_lastName, user_maidenName, user_age, user_gender, user_email, user_phone, user_username, user_password, user_birthDate, user_image, user_height, user_weight, user_eyeColor, user_hair_type, user_hair_color, user_university, user_rol FROM users WHERE user_id = UUID_TO_BIN(?);`,
+      [uuid]
+    );
+
+    return userCreated;
+  };
+
+  static updateUser = async (id, updateUser) => {
+    const filterUpdatedElements = Object.entries(updateUser).filter(
+      ([key]) => updateUser[key] !== undefined
+    );
+
+    const setValue = filterUpdatedElements
+      .map(([key]) => `user_${key} = ?`)
+      .join(", ");
+    console.log(setValue);
+
+    const values = filterUpdatedElements.map(([_, value]) => value);
+
+    try {
+      await connection.query(
+        `
+      UPDATE users
+      SET ${setValue}
+      WHERE user_id = UUID_TO_BIN(?);
+      `,
+        [...values, id]
+      );
+    } catch (e) {
+      throw new Error("Error updating user: " + e.message);
+    }
+
+    const [result] = await connection.query(
+      `
+      SELECT * FROM users
+      WHERE user_id = UUID_TO_BIN(?);
+      `,
+      [id]
+    );
+
+    return result;
+  };
+
+  static deleteUser = async (id) => {
+    const [result] = await connection.query(
+      `DELETE FROM users
+      WHERE user_id = UUID_TO_BIN(?);
+      `,
+      [id]
+    );
+
+    return result;
+  };
 }
