8ef44e1b407677b406b6630942db7cbfdba998b4
Arianna <ariigrangetto5@gmail.com>
2025-11-07 00:56:59 -0300

    feat: try catch

--- client/src/service/getUsers.js
+++ client/src/service/getUsers.js
@@ -1,7 +1,11 @@
 const API_URL = import.meta.env.VITE_API_URL; // Replace with your server's port number
 
 export async function getUsers() {
-  const result = await fetch(`${API_URL}/users`);
-  const data = await result.json();
-  return data.users;
+  try {
+    const result = await fetch(`${API_URL}/users`);
+    const data = await result.json();
+    return data.users;
+  } catch (e) {
+    console.error("Error fetching data" + e.message);
+  }
 }
